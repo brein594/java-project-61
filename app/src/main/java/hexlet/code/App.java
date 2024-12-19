6baf3e98fa63bd30b141cf5894fe772349b6f32a
brein594 <brein594@gmail.com>
2024-12-19 13:36:49 +0300

    add game calc

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,14 +8,19 @@ public class App {
         int play = 1;
         while (play != 0 ) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("2 - Even");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         Scanner scanner = new Scanner(System.in);
         play = scanner.nextInt();
         System.out.println("Your choice: " + play);
         //scanner.close();
             switch (play) {
+                case 3:
+                    System.out.println("Welcome to the Brain Games!");
+                    Game.calc();
+                    break;
                 case 2:
                     System.out.println("Welcome to the Brain Games!");
                     Game.even();
--- app/src/main/java/hexlet/code/Game.java
+++ app/src/main/java/hexlet/code/Game.java
@@ -1,4 +1,5 @@
 package hexlet.code;
+import java.util.Random;
 
 
 import java.util.Scanner;
@@ -25,6 +26,52 @@ public class Game {
                 indexGame++;
             } else {
                 System.out.println(question + " is wrong answer ;(. Correct answer was " + evenNumber);
+                System.out.println("Let's try again, " + playName + "!");
+                indexGame = endGame;
+            }
+            if (indexGame == 3) {
+                System.out.println("Congratulations, " + playName + "!");
+            }
+        }
+    }
+    public static void calc() {
+        var playName = Cli.getByName();
+        int endGame = 4;
+        System.out.println("What is the result of the expression?");
+        int question;
+        int indexGame = 0;
+        while (indexGame < 3) {
+            var numberRandomA = (int)(Math.random() * 100) +1;
+            var numberRandomB = (int)(Math.random() * 100) +1;
+            Random random = new Random();
+            int numberRandomOperation = random.nextInt(2);
+            int resultInt = 0;
+            String resultString = "";
+            switch (numberRandomOperation) {
+                case (0):
+                    resultInt = numberRandomA + numberRandomB;
+                    resultString = "+";
+                    break;
+                case (1):
+                    resultInt = numberRandomA - numberRandomB;
+                    resultString = "-";
+                    break;
+                case (2):
+                    resultInt = numberRandomA * numberRandomB;
+                    resultString = "*";
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("Question: " + numberRandomA + " " + resultString + " " + numberRandomB);
+            System.out.println("Your answer: ");
+            Scanner scanner = new Scanner(System.in);
+            question = scanner.nextInt();
+            if (question == resultInt) {
+                System.out.println("Correct!");
+                indexGame++;
+            } else {
+                System.out.println(question + " is wrong answer ;(. Correct answer was " + resultInt);
                 System.out.println("Let's try again, " + playName + "!");
                 indexGame = endGame;
             }
