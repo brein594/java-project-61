ef71e6622598b51dc18c48e0364eb69fe26a8598
brein594 <brein594@gmail.com>
2024-12-20 13:37:26 +0300

    add game Progress and asciinema

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,12 +11,16 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
         Scanner scanner = new Scanner(System.in);
         play = scanner.nextInt();
         System.out.println("Your choice: " + play);
         //scanner.close();
             switch (play) {
+                case 5:
+                    Game.progression();
+                    break;
                 case 4:
                     Game.gcd();
                     break;
--- app/src/main/java/hexlet/code/games/Game.java
+++ app/src/main/java/hexlet/code/games/Game.java
@@ -1,6 +1,8 @@
 package hexlet.code.games;
 import hexlet.code.Cli;
 import hexlet.code.Engine;
+
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,6 +12,7 @@ public class Game {
         Engine.greeting();
         Cli.getByName();
     }
+
     public static void even() {
         Engine.greeting();
         var playName = Cli.getByName();
@@ -35,6 +38,7 @@ public class Game {
             }
         }
     }
+
     public static void calc() {
         Engine.greeting();
         var playName = Cli.getByName();
@@ -110,7 +114,53 @@ public class Game {
             }
         }
     }
+
     private static int findGcd(int a, int b) {
         return (a % b == 0) ? Math.abs(b) : findGcd(b, a % b);
+    }
+
+    public static void progression() {
+        Engine.greeting();
+        var playName = Cli.getByName();
+        System.out.println("What number is missing in the progression?");
+        int answer;
+        int indexGame = 0;
+        while (indexGame < Engine.numbersOfGames) {
+            var numberRandomA1 = (int)(Math.random() * 100) +1;
+            var stepProgress = (int)(Math.random() * 100) +1;
+            var numberRandomPosition = (int)(Math.random() * 10);
+            int[] arifProgress= new int[10];
+            arifProgress[0] = numberRandomA1;
+            for (int i = 1; i < 10; i++){
+                arifProgress[i] = arifProgress[0] + i * stepProgress;
+            }
+            int resultInt = arifProgress[numberRandomPosition];
+            System.out.print("Question: ");
+            for (int index : Arrays.copyOfRange(arifProgress, 0, numberRandomPosition)) {
+                System.out.print(index + " ");
+            }
+            System.out.print(".." + " ");
+            if (numberRandomPosition < arifProgress.length - 1 ) {
+                for (int index : Arrays.copyOfRange(arifProgress, numberRandomPosition + 1, arifProgress.length-1)) {
+                    System.out.print(index + " ");
+                }
+            }
+            System.out.println("");
+            System.out.print("Your answer: ");
+
+            Scanner scanner = new Scanner(System.in);
+            answer = scanner.nextInt();
+
+            if (answer == resultInt) {
+                Engine.correct();
+                indexGame++;
+            } else {
+                Engine.failEnd(answer, resultInt, playName);
+                indexGame = Engine.numbersOfGames + 1;
+            }
+            if (indexGame == Engine.numbersOfGames) {
+                Engine.succesfulEnd(playName);
+            }
+        }
     }
 }
