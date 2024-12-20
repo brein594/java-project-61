90ead29cab211456ba8a28f1d741bc9d9162ad20
brein594 <brein594@gmail.com>
2024-12-21 00:18:04 +0300

    fix style

--- app/src/main/java/hexlet/code/games/Game.java
+++ app/src/main/java/hexlet/code/games/Game.java
@@ -168,7 +168,8 @@ public class Game {
         Engine.greeting();
         var playName = Cli.getByName();
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-        int[] primeNumber = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        int[] primeNumber = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+                             61, 67, 71, 73, 79, 83, 89, 97};
         String answer = "";
         var answerSimple = "no";
         int indexGame = 0;
