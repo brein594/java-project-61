b39c9ad6d6d0ababa7479866b755320a8baa1a83
brein594 <brein594@gmail.com>
2024-12-22 20:06:10 +0300

    add bag simle number 65 PrimeGame

--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -10,11 +10,11 @@ public class PrimeGame {
         var playName = Cli.getByName();
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
         final int[] primeNumber = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
-                                   61, 67, 71, 73, 79, 83, 89, 97};
+                                   61, 65, 67, 71, 73, 79, 83, 89, 97};
         String answer;
         var answerSimple = "no";
         int indexGame = 0;
-        final var numberLoser = 1;
+        final int numberLoser = 1;
         while (indexGame < Engine.NUMBER_OF_GAMES) {
             final int rangeMax = 100;
             final int rangeFirst = 1;
@@ -38,7 +38,6 @@ public class PrimeGame {
             }
             if (indexGame == Engine.NUMBER_OF_GAMES) {
                 Engine.showWinMessage(playName);
-                //System.out.println("Let's try again, " + playName + "!");
             }
         }
     }
